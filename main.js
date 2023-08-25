5782f4c172bda9a33eeb4d1bdc4579b484e52f20
Md Sholayman <mdsholayman998@gmail.com>
2023-08-26 01:00:27 +0600

    template card has been implimented

--- main.js
+++ main.js
@@ -11,11 +11,46 @@ const url = require("url");
 //dependencies
 const slugify = require('slugify');
 
+//importing local modules
+
+const replaceTemplates = require("./modules/replaceTemplate");
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8") ;
+const tempProduct=fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
+//importing data
+const data = fs.readFileSync(`${__dirname}/data/data.json`,"utf-8")
+const jsonData = JSON.parse(data)
+
+//slugify
+
+const slugs = jsonData.map(el =>slugify(el.productName,{lower:true}));
+
+
+
 
 
 //creating server
 
 const server = http.createServer((req,res)=>{
+
+const {pathname,query} = url.parse(req.url,true)
+// const pathName = req.url;
+
+
+if(pathname === "/" || pathname === "/overview"){
+    res.writeHead(200,{
+        "Content-Type" : "text/html"
+    });
+
+    //running a loop in data[array of object]
+
+const cardHtml = jsonData.map(el => replaceTemplates(tempCard,el)).join('');
+
+const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml) ;
+
+res.end(output)
+}
+
 
 })
 
@@ -23,7 +58,7 @@ const server = http.createServer((req,res)=>{
 
 //listening to the server
 
-server.listen(8000,()=>{
+server.listen(8080,()=>{
     console.log('server is running......');
 })
 
